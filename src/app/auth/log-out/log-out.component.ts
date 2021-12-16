68e6273d9dd5d2ad18d1a02e0556fa6b67144b1b
Lekso Aleksidze <65183181+LeksoAleksidze@users.noreply.github.com>
2021-12-16 10:40:11 +0400

    add

--- src/app/auth/log-out/log-out.component.ts
+++ src/app/auth/log-out/log-out.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { from } from 'rxjs';
 import { finalize } from 'rxjs/operators';
+
 import { Router } from '@angular/router';
 import { LoadingService } from 'src/app/services/loading.service';
 
@@ -13,6 +14,8 @@ import { LoadingService } from 'src/app/services/loading.service';
   styleUrls: ['./log-out.component.scss'],
 })
 export class LogOutComponent implements OnInit {
+  textTitle: string = '';
+  addSuccess = false;
   constructor(
     private auth: AuthService,
     private router: Router,
@@ -20,9 +23,36 @@ export class LogOutComponent implements OnInit {
   ) {}
 
   ngOnInit() {}
+  closePopUp(e: boolean) {
+    this.addSuccess = false;
+  }
 
   register(form: NgForm) {
     if (!form.valid) {
+      return;
+    }
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!form.value.email.match(mailformat)) {
+      this.addSuccess = true;
+      const lang = localStorage.getItem('lang');
+      if (lang === 'ka') {
+        this.textTitle = 'შეიყვანეთ სწორი მეილი';
+      } else {
+        this.textTitle = 'Enter the correct email';
+      }
+
+      return;
+    }
+
+    if (form.value.password.length < 6) {
+      this.addSuccess = true;
+      const lang = localStorage.getItem('lang');
+      if (lang === 'ka') {
+        this.textTitle = 'პაროლი უნდა შედგებოდეს მინიმუმ 6 სიმბოლოსგან';
+      } else {
+        this.textTitle = 'The password must be at least 6 characters long';
+      }
+
       return;
     }
 
